cc13d9501e6dbd8813c055a4b3a4e121a0dc472c
Anita Tabaczka <anitatabacka@gmail.com>
2021-06-20 03:01:19 +0200

    dataset loader update

--- emotion_detector/dataset_loader.py
+++ emotion_detector/dataset_loader.py
@@ -19,7 +19,7 @@ os.makedirs(os.path.join('data',outer_name), exist_ok=True)
 for inner_name in inner_names:
     os.makedirs(os.path.join('data',outer_name,inner_name), exist_ok=True)
 
-# do zliczania kazdej kategorii
+# do zliczania zdjęć kazdej kategorii od 0
 angry = 0
 disgusted = 0
 fearful = 0
@@ -35,17 +35,19 @@ sad_test = 0
 surprised_test = 0
 neutral_test = 0
 
-df = pd.read_csv('./fer2013/fer2013.csv')
+#zliczanie ilość plików z 'Training' Usage do pętli
+data = pd.read_csv('./fer2013/fer2013.csv')
+x = int(data['Usage'].value_counts()[0:1])
 mat = np.zeros((48,48),dtype=np.uint8)
 print("Saving images...")
-
+bytes = 48*48
 # czytanie pliku csv z datasetem
-for i in tqdm(range(len(df))):
-    txt = df['pixels'][i]
+for i in tqdm(range(len(data))):
+    txt = data['pixels'][i]
     words = txt.split()
     
 # ustawienie rozmiaru zdjęć na 48x48
-    for j in range(2304):
+    for j in range(bytes):
         xind = j // 48
         yind = j % 48
         mat[xind][yind] = atoi(words[j])
@@ -53,26 +55,26 @@ for i in tqdm(range(len(df))):
     img = Image.fromarray(mat)
 
 # rozpakowywanie datasetu do podfolderów
-    if i < 28709:
-        if df['emotion'][i] == 0:
+    if i < x:
+        if data['emotion'][i] == 0:
             img.save('data/emotions/angry/im'+str(angry)+'.png')
             angry += 1
-        elif df['emotion'][i] == 1:
+        elif data['emotion'][i] == 1:
             img.save('data/emotions/disgusted/im'+str(disgusted)+'.png')
             disgusted += 1
-        elif df['emotion'][i] == 2:
+        elif data['emotion'][i] == 2:
             img.save('data/emotions/fearful/im'+str(fearful)+'.png')
             fearful += 1
-        elif df['emotion'][i] == 3:
+        elif data['emotion'][i] == 3:
             img.save('data/emotions/happy/im'+str(happy)+'.png')
             happy += 1
-        elif df['emotion'][i] == 4:
+        elif data['emotion'][i] == 4:
             img.save('data/emotions/sad/im'+str(sad)+'.png')
             sad += 1
-        elif df['emotion'][i] == 5:
+        elif data['emotion'][i] == 5:
             img.save('data/emotions/surprised/im'+str(surprised)+'.png')
             surprised += 1
-        elif df['emotion'][i] == 6:
+        elif data['emotion'][i] == 6:
             img.save('data/emotions/neutral/im'+str(neutral)+'.png')
             neutral += 1
 
