f80bd5e1fb6dcc6117a96ac4543295524a186df4
Anita Tabaczka <anitatabacka@gmail.com>
2021-06-28 16:38:39 +0200

    main vol 2

--- emotion_detector/main.py
+++ emotion_detector/main.py
@@ -41,14 +41,14 @@ elif mode == "start":
         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 0, 255), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame, emotion_dict[maxindex], (x+80, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+        cv2.imshow('Video', cv2.resize(frame,(1600,1600),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
